84c653d323450819ad093d83da48525cfcab0268
gupta-Skdisha <153117005+gupta-Skdisha@users.noreply.github.com>
2026-07-15 18:14:42 +0530

    Time: 1 ms (83.11%) | Memory: 42.4 MB (86.19%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int sumOdd=0;
+        int sumEven=0;
+
+        if(n/2==0){
+            sumEven+=n;
+            
+        }
+        sumOdd+=n;
+        return n;
+        
+    }
+}
