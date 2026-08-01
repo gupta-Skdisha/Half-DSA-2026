55e274335f6404da315aaf482d76f56bbc2a9d19
gupta-Skdisha <153117005+gupta-Skdisha@users.noreply.github.com>
2026-08-01 23:26:12 +0530

    Time: 65 ms (28.69%) | Memory: 43.1 MB (10.58%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solve(int[] nums,int i,int j) {
+        
+        if(i==j){
+           return nums[i];
+        }
+
+        int left=nums[i] - solve(nums,i+1,j);
+        int right=nums[j]-solve(nums,i,j-1);
+
+        return Math.max(left,right);
+    }
+
+     public boolean predictTheWinner(int[] nums) {
+        return solve(nums, 0, nums.length - 1) >= 0;
+    }
+    }
